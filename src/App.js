c370c33d1d99529460f13b26537ee1580f3f4f44
Insaner <dbfpzk2002@gmail.com>
2023-01-12 23:36:53 +0900

    filling learn_record by nomad source code. Fuckgit add .

--- src/App.js
+++ src/App.js
@@ -3,19 +3,40 @@ import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 //useEffect(인자1 한번만 실행하고픈함수, ??)
 function App() {
-  const [counter, setValue] = useState(0);
-
-  const OnClick = () => setValue((prev) => prev + 1);
-  console.log("all time call api"); //렌더링중 api를 매번 불러오지 않도록 제어하고 싶을때
-
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    console.log("call api once");
+    getMovies();
   }, []);
   return (
     <div>
-      <h1 className={styles.title}>{counter}</h1>
-      <Button text={"Continue"} />
-      <button onClick={OnClick}>Clickme</button>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
