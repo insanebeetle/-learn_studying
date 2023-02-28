fb840e4ae7698b0ad7b52ad43b107a1d468ec42a
Insaner <dbfpzk2002@gmail.com>
2023-02-28 16:53:34 +0900

    vaniila-redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+//아래처럼 파라미터로 티폴트,초기화 값 입력
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+}; //if문에서 switch문으로 리팩트
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
+//이벤트 리스너를 구현하는 두가지 방법
