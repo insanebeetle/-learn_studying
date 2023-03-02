43f744f5300090a88bae2a0b1c0effb0fd16aaed
Insaner <dbfpzk2002@gmail.com>
2023-03-02 15:28:52 +0900

    react-redux-todo old-style

---
+++ src/routes/Home.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import ToDo from "../component/ToDo";
+import { actionCreators } from "../store";
+
+function Home({ toDos, addToDo }) {
+  const [text, setText] = useState("");
+
+  function onChange(e) {
+    setText(e.target.value);
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+    addToDo(text);
+    setText("");
+  }
+
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button type="submit">ADD</button>
+      </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo {...toDo} key={toDo.id} />
+        ))}
+      </ul>
+    </>
+  );
+}
+
+//이거는 기존 pros에 새로운 state를 추가할수 있음
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
